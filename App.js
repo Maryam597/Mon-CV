2eea7c30d181b77a63f2cfd369f5a195f9bce1c4
Maryam597 <maryam.dri@hotmail.fr>
2023-05-25 11:54:28 +0200

    new version

---
+++ App.js
@@ -0,0 +1,18 @@
+
+const darkmode = document.getElementById('toggle');
+const cvgauche = document.getElementById('leftside')
+const cvdroite = document.getElementById('rightside')
+const pres = document.getElementById('introduction')
+
+ 
+
+darkmode.onclick = function(){
+   darkmode.classList.toggle('active'),
+   cvgauche.classList.toggle('active');
+   cvdroite.classList.toggle('active');
+   pres.classList.toggle('active');
+
+
+}
+
+console.log("yo")
