e03f162a01b4328128cf27c1923967f39153cfb7
Maryam597 <maryam.dri@hotmail.fr>
2023-05-26 11:09:04 +0200

    ok

--- App.js
+++ App.js
@@ -1,18 +1,15 @@
 
 const darkmode = document.getElementById('toggle');
-const cvgauche = document.getElementById('leftside')
+const cvgauche = document.getElementById('leftside');
 const cvdroite = document.getElementById('rightside')
-const pres = document.getElementById('introduction')
-
+const intro = document.getElementById('introduction')
  
 
 darkmode.onclick = function(){
    darkmode.classList.toggle('active'),
    cvgauche.classList.toggle('active');
-   cvdroite.classList.toggle('active');
-   pres.classList.toggle('active');
-
-
+   cvdroite.classList.toggle('active')
+   intro.classList.toggle('active')
 }
 
 
